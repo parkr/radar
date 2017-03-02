2f1ca8d2d6fbe093c81d879bfd8ce187188f3e9e
Parker Moore <parkrmoore@gmail.com>
2017-03-02 15:51:02 -0500

    pull out the body of the message from body-plain

--- email.go
+++ email.go
@@ -1,9 +1,11 @@
 package radar
 
 import (
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/mvdan/xurls"
 )
 
 type EmailHandler struct {
@@ -11,23 +13,31 @@ type EmailHandler struct {
 }
 
 func (h EmailHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	log.Printf("%#v", r)
-
-	stuff, err := ioutil.ReadAll(r.Body)
-	r.Body.Close()
-
-	if err != nil {
-		log.Println("error processing payload", err)
-		http.Error(w, "error processing payload "+err.Error(), http.StatusInternalServerError)
+	if contentType := r.Header.Get("Content-Type"); contentType != "application/x-www-form-urlencoded" {
+		log.Println("Don't know how to handle Content-Type:", contentType)
+		http.Error(w, "cannot process Content-Type: "+contentType, http.StatusBadRequest)
 		return
 	}
 
-	payload := string(stuff)
-
+	emailBody := r.FormValue("body-plain")
 	if h.Debug {
-		log.Printf("%#v", payload)
+		log.Printf("body-plain: %#v", emailBody)
 	}
 
-	http.Error(w, "hehe", http.StatusOK)
+	var urls []string
+	if matches := xurls.Strict.FindAllString(emailBody, -1); matches != nil && len(matches) > 0 {
+		urls = append(urls, matches...)
+	}
+
+	if len(urls) == 0 {
+		log.Println("no urls in body: ", emailBody)
+		http.Error(w, "no urls present in email body", http.StatusOK)
+		return
+	}
+
+	if h.Debug {
+		log.Printf("urls: %#v", urls)
+	}
+
+	http.Error(w, fmt.Sprintf("added %d urls to today's radar", len(urls)), http.StatusCreated)
 }
