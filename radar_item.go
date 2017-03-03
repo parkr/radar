016e511c7b6098f8f1bd6e85265d5a617aeb0008
Parker Moore <parkrmoore@gmail.com>
2017-03-03 16:51:02 -0500

    Provide more descriptive errors from database interactions.

--- radar_item.go
+++ radar_item.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"log"
 	"strconv"
+
+	"github.com/pkg/errors"
 )
 
 // RadarItem is a single row in the radar_items table. It contains a URL and optionally a title.
@@ -32,7 +34,7 @@ type RadarItemsService struct {
 func (rs RadarItemsService) List(ctx context.Context, limit int) ([]RadarItem, error) {
 	tx, err := rs.Database.BeginTx(ctx, nil)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "transaction failed to begin")
 	}
 	defer tx.Rollback()
 
@@ -42,7 +44,7 @@ func (rs RadarItemsService) List(ctx context.Context, limit int) ([]RadarItem, e
 
 	rows, err := tx.Query("SELECT id, url, title FROM radar_items LIMIT 0,?", limit)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "query for select failed")
 	}
 	defer rows.Close()
 
@@ -50,14 +52,14 @@ func (rs RadarItemsService) List(ctx context.Context, limit int) ([]RadarItem, e
 	for rows.Next() {
 		var item RadarItem
 		if err := rows.Scan(&item.ID, &item.URL, &item.Title); err != nil {
-			return nil, err
+			return nil, errors.Wrap(err, "scan for select failed")
 		}
 		log.Printf("loaded row=%#v", item)
 		items = append(items, item)
 	}
 
 	if err = tx.Commit(); err != nil {
-		return items, err
+		return items, errors.Wrap(err, "commit for select failed")
 	}
 
 	return items, nil
@@ -67,23 +69,23 @@ func (rs RadarItemsService) List(ctx context.Context, limit int) ([]RadarItem, e
 func (rs RadarItemsService) Create(ctx context.Context, m RadarItem) error {
 	tx, err := rs.Database.BeginTx(ctx, nil)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "transaction failed to begin")
 	}
 	defer tx.Rollback()
 
 	stmt, err := tx.Prepare("INSERT INTO radar_items (url, title) VALUES (?, ?)")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "prepare for insert failed")
 	}
 
 	if _, err = tx.Exec(m.URL, m.Title); err != nil {
-		return err
+		return errors.Wrap(err, "exec for insert failed")
 	}
 	defer stmt.Close()
 
 	err = tx.Commit()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "commit for insert failed")
 	}
 
 	return nil
@@ -93,22 +95,22 @@ func (rs RadarItemsService) Create(ctx context.Context, m RadarItem) error {
 func (rs RadarItemsService) Delete(ctx context.Context, id int64) error {
 	tx, err := rs.Database.BeginTx(ctx, nil)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "transaction failed to begin")
 	}
 	defer tx.Rollback()
 
 	stmt, err := tx.Prepare("DELETE FROM radar_items WHERE id = ?")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "prepare for delete failed")
 	}
 	if _, err = tx.Exec(strconv.FormatInt(id, 10)); err != nil {
-		return err
+		return errors.Wrap(err, "exec for delete failed")
 	}
 	defer stmt.Close()
 
 	err = tx.Commit()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "commit for delete failed")
 	}
 
 	return nil
