c5e8d5892ec2480927cd61396d1a9ffc2e940d5a
Parker Moore <parkrmoore@gmail.com>
2017-03-04 17:11:52 -0500

    Generate a radar on SIGUSR2 signal.

--- cmd/radar/main.go
+++ cmd/radar/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -31,7 +32,7 @@ func getRadarItemsService() radar.RadarItemsService {
 	return radar.RadarItemsService{Database: getDB()}
 }
 
-func generateRadar(radarItemsService radar.RadarItemsService, ticker *time.Ticker, hourToGenerateRadar string) {
+func radarGenerator(radarItemsService radar.RadarItemsService, ticker *time.Ticker, hourToGenerateRadar string) {
 	if len(hourToGenerateRadar) != 2 {
 		log.Printf("NOT generating radar. Hour to generate is not in 24-hr time: '%s'", hourToGenerateRadar)
 		ticker.Stop()
@@ -58,15 +59,19 @@ func generateRadar(radarItemsService radar.RadarItemsService, ticker *time.Ticke
 		thisHour := time.Now().Format("15")
 		if thisHour == hourToGenerateRadar {
 			log.Println("The time has come: let's generate the radar!")
-			issue, err := radar.GenerateRadarIssue(radarItemsService, githubToken, radarRepo)
-			if err == nil {
-				log.Printf("Generated new radar issue: %s", *issue.HTMLURL)
-			} else {
-				log.Printf("Couldn't generate new radar issue: %#v", err)
-			}
+			generateRadar(radarItemsService, githubToken, radarRepo)
 		} else {
 			log.Printf("Wrong hour to generate! %s != %s", thisHour, hourToGenerateRadar)
 		}
+	}
+}
+
+func generateRadar(radarItemsService radar.RadarItemsService, githubToken, radarRepo string) {
+	issue, err := radar.GenerateRadarIssue(radarItemsService, githubToken, radarRepo)
+	if err == nil {
+		log.Printf("Generated new radar issue: %s", *issue.HTMLURL)
+	} else {
+		log.Printf("Couldn't generate new radar issue: %#v", err)
 	}
 }
 
@@ -95,7 +100,15 @@ func main() {
 	go emailHandler.Start()
 
 	ticker := time.NewTicker(1 * time.Hour)
-	go generateRadar(radarItemsService, ticker, hourToGenerateRadar)
+	go radarGenerator(radarItemsService, ticker, hourToGenerateRadar)
+	radarC := make(chan os.Signal, 1)
+	signal.Notify(radarC, syscall.SIGUSR2)
+	go func() {
+		for sig := range radarC {
+			log.Printf("Received signal %#v!", sig)
+			generateRadar(radarItemsService, os.Getenv("GITHUB_ACCESS_TOKEN"), os.Getenv("RADAR_REPO"))
+		}
+	}()
 
 	log.Println("Starting server on", binding)
 	server := &http.Server{Addr: binding}
@@ -108,6 +121,7 @@ func main() {
 			log.Printf("Received signal %#v!", sig)
 			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 			defer cancel()
+			close(radarC)
 			ticker.Stop()
 			log.Println("Closing database connection...")
 			radarItemsService.Shutdown(ctx)
