c3480207e3f5d0b2aae364e7f339cc64b1a10bfd
Parker Moore <parkrmoore@gmail.com>
2020-12-28 11:11:43 -0500

    Add isPrivateHost() check to ensure fetching a webpage doesn't expose private resources

--- parser_test.go
+++ parser_test.go
@@ -24,3 +24,41 @@ func Test_isBinaryResource(t *testing.T) {
 		}
 	}
 }
+
+func Test_isPrivateHost(t *testing.T) {
+	testcases := map[string]bool{
+		"localhost":       true,
+		"127.0.0.1":       true,  // IPv4 loopback
+		"10.0.0.1":        true,  // RFC1918
+		"172.16.0.1":      true,  // RFC1918
+		"192.168.0.1":     true,  // RFC1918
+		"169.254.0.1":     true,  // RFC3927 link-local
+		"::1":             true,  // IPv6 loopback
+		"fe80::":          true,  // IPv6 link-local
+		"fc00::":          true,  // IPv6 unique local addr
+		"129.129.129.129": false, // public
+		"0.0.0.0":         false,
+	}
+	for input, expected := range testcases {
+		actual := isPrivateHost(input)
+		if actual != expected {
+			t.Fatalf("expected %q to yield %t, got %t", input, expected, actual)
+		}
+	}
+}
+
+func Test_titleForWebpage(t *testing.T) {
+	testcases := map[string]string{
+		"http://localhost/foo/bar":        "A private page on localhost",
+		"http://server.local/foo/bar":     "A private page on server.local",
+		"https://myapp.localhost/foo/bar": "A private page on myapp.localhost",
+		"http://127.0.0.1:1234/foo/bar":   "A private page on 127.0.0.1",
+		"https://example.com":             "Example Domain",
+	}
+	for input, expected := range testcases {
+		actual := titleForWebpage(input)
+		if actual != expected {
+			t.Fatalf("expected %q to yield %q, got %q", input, expected, actual)
+		}
+	}
+}
